7dd51a44a24aa84e7240b8a1acea43cb3e761b07
adam <itstudy@kakao.com>
2024-05-08 11:31:51 +0900

    update

--- apiserver/views.py
+++ apiserver/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 def index(request):
     data = {"result":"success", "data": [
         {"id":"시모나", "name":"희정"},
-        {"id":"군계", "name":"아담"}
+        {"id":"카푸치노", "name":"애영"}
     ]}
 
     return Response(data, status=status.HTTP_200_OK)
